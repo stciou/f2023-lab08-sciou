c141fc1deb39e93d732c02b7a2136218d5b052c2
Shang-Ting Ciou <sciou@andrew.cmu.edu>
2023-10-25 13:20:14 -0400

    Reimplemented the Promise code as async/await

--- src/index.ts
+++ src/index.ts
@@ -2,44 +2,28 @@ import { ImageAnnotatorClient } from '@google-cloud/vision';
 
 const client = new ImageAnnotatorClient();
 
-/**
- * Runs logo detection on the given list of file names and logs the description and average score of each logo.
- * @param fileNames - An array of file names to run logo detection on.
- * @returns void
- */
-function main (fileNames: string[]): void {
-    fileNames.forEach((fileName: string) => {
-        console.log(`Running logo detection on ${fileName}`);
-        client.logoDetection(fileName)
-        .then(([result]) => {
+async function mainAsync(fileNames: string[]): Promise<void> {
+    for (const fileName of fileNames) {
+        try {
+            console.log(`Running logo detection on ${fileName}`);
+            const [result] = await client.logoDetection(fileName);
             let scores: number[] = [];
             const logos = result.logoAnnotations;
             logos?.forEach((logo) => {
                 if (logo.description)
-                    console.log(`"${logo.description}" found in in file ${fileName}`);
+                    console.log(`"${logo.description}" found in file ${fileName}`);
                 if (logo.score)
                     scores.push(logo.score);
             });
-            const avg = scores.reduce((a, b) => a + b) / scores.length;
+            const avg = scores.reduce((a, b) => a + b, 0) / scores.length;
             console.log(`Average score for ${fileName}: ${avg}`);
-        })
-        .catch((err) => {
+        } catch (err) {
             if (err.code === 'ENOENT')
                 console.log(`File ${fileName} not found`);
-        });
-    });
-}
-
-main([
-    './images/cmu.jpg', 
-    './images/logo-types-collection.jpg', 
-    './images/not-a-file.jpg'
-]);
-
-// Implement the async version of the above here
-// Your version should not use .then and should use try/catch instead of .catch
-async function mainAsync(fileNames: string[]): Promise<void> {
-    // Your code here
+            else
+                console.error(`Error processing ${fileName}:`, err);
+        }
+    }
 }
 
 mainAsync([
